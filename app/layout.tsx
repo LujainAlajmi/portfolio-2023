902e15f095b860495d687822736508953f7d4d60
Lujain <lujainmalajmi@gmail.com>
2023-07-16 13:00:47 +0300

    analytics and meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,50 @@
 import "./globals.css";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import "cal-sans";
+import { Analytics } from "@/components/ui/analytics";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Lujain Alajmi",
   description: "Your friendly neighborhood nerd.",
+  keywords: [
+    "lujain",
+    "alajmi",
+    "lujain alajmi",
+    "lujainalajmi",
+    "react",
+    "riyadh",
+    "KSA",
+  ],
+  authors: {
+    name: "Lujain Alajmi",
+    url: "https://www.lujain.dev/",
+  },
+  creator: "Lujain Alajmi",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.lujain.dev/",
+    title: "Lujain Alajmi",
+    description: "Your friendly neighborhood nerd.",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lujain Alajmi",
+    description: "Your friendly neighborhood nerd.",
+    images: [`https://www.lujain.dev/og.jpg`],
+    creator: "lujainalajmi",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `https://www.lujain.dev/site.webmanifest`,
 };
 
 export default function RootLayout({
@@ -29,6 +65,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
